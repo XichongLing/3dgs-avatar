aef70980945357ce485d059d1865a1c2802dfa14
Xichong Ling <lingxi@ait-server-04.inf.ethz.ch>
2024-09-19 13:09:52 +0200

    4dr dataloader

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,9 +1,11 @@
 from .zjumocap import ZJUMoCapDataset
 from .people_snapshot import PeopleSnapshotDataset
+from .fd_dress import FDressDataset
 
 def load_dataset(cfg, split='train'):
     dataset_dict = {
         'zjumocap': ZJUMoCapDataset,
         'people_snapshot': PeopleSnapshotDataset,
+        '4Dress': FDressDataset,
     }
     return dataset_dict[cfg.name](cfg, split)
